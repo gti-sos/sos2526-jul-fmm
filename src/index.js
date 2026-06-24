76d200d8e06fe5efc485c7d035d00b5b6364451f
felmormar <felipe@Delirio.localdomain>
2026-06-24 13:43:17 +0200

    render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import path from 'path';
-
+import { handler } from './src/front/build/handler.js';
 import { loadBackendFMM, loadBackendFMM_v2, loadIntegrationsProxy } from './api-fmm.js';
 
 const app = express();
@@ -11,13 +10,11 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Servir los archivos estáticos del frontend de Svelte (generados en la carpeta dist por Vite)
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'src', 'front', 'dist'))); 
-
 loadBackendFMM(app);
 loadBackendFMM_v2(app);
 loadIntegrationsProxy(app);
+
+app.use(handler);
 
 app.listen(port, () => {
     console.log(`🚀 Servidor API funcionando y escuchando en el puerto ${port}`);
